2ac30d1334ba7a32d380abc07a96c4467f5ae212
markehlkedialexa <mark.ehlke@dialexa.com>
2023-11-17 09:35:43 -0600

    Added formatting config

--- src/main/java/com/example/springbootlearning/SpringbootlearningApplication.java
+++ src/main/java/com/example/springbootlearning/SpringbootlearningApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootlearningApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootlearningApplication.class, args);
 	}
-
 }
